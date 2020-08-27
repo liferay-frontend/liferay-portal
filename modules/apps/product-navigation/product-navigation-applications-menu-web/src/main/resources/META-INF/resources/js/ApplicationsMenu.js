653bbd4c5bf2ee2aa76e18ede177e7ab2a0111c7
Carlos Lancha <carlos.lancha@liferay.com>
2020-08-27 11:43:51 +0200

    LPS-118408 Replaced sites to environments

--- modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/ApplicationsMenu.js
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/ApplicationsMenu.js
@@ -26,6 +26,27 @@ import {fetch, navigate, openSelectionModal} from 'frontend-js-web';
 import PropTypes from 'prop-types';
 import React, {useRef, useState} from 'react';
 
+const EnvironmentsPanel = ({portletNamespace, sites}) => {
+	return (
+		<div className="applications-menu-environments c-p-3 c-px-md-4">
+			<h2 className="applications-menu-environments-label c-mt-2 c-mt-md-0">
+				{Liferay.Language.get('environments')}
+			</h2>
+
+			<ul className="c-my-2 list-unstyled">
+				{sites && (
+					<Sites
+						mySites={sites.mySites}
+						portletNamespace={portletNamespace}
+						recentSites={sites.recentSites}
+						viewAllURL={sites.viewAllURL}
+					/>
+				)}
+			</ul>
+		</div>
+	)
+};
+
 const Site = ({current, label, logoURL, url}) => {
 	return (
 		<li className="c-mt-3">
@@ -54,9 +75,15 @@ const Site = ({current, label, logoURL, url}) => {
 	);
 };
 
-const SitesPanel = ({mySites, portletNamespace, recentSites, viewAllURL}) => {
+const Sites = ({mySites, portletNamespace, recentSites, viewAllURL}) => {
 	return (
 		<>
+			<li className="c-my-3">
+				<h2 className="applications-menu-nav-header">
+					{Liferay.Language.get('sites')}
+				</h2>
+			</li>
+
 			{recentSites?.length > 0 &&
 				recentSites.map(({current, key, label, logoURL, url}) => (
 					<Site
@@ -258,22 +285,10 @@ const AppsPanel = ({
 							lg="3"
 							md="4"
 						>
-							<div className="applications-menu-sites c-p-3 c-px-md-4">
-								<h2 className="applications-menu-sites-label c-mt-2 c-mt-md-0">
-									{Liferay.Language.get('sites')}
-								</h2>
-
-								<ul className="c-mb-2 list-unstyled">
-									{sites && (
-										<SitesPanel
-											mySites={sites.mySites}
-											portletNamespace={portletNamespace}
-											recentSites={sites.recentSites}
-											viewAllURL={sites.viewAllURL}
-										/>
-									)}
-								</ul>
-							</div>
+							<EnvironmentsPanel
+								portletNamespace={portletNamespace}
+								sites={sites}
+							/>
 						</ClayLayout.Col>
 					</ClayLayout.Row>
 				</ClayLayout.ContainerFluid>
