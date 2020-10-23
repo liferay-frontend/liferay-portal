75be202006ba570e4a3e40bc484b3c9033d53d84
Greg Hurrell <greg.hurrell@liferay.com>
2020-10-23 19:11:48 +0200

    LPS-121753 Fix no-use-before define violations in dynamic-data-mapping-form-builder
    Fixes:
    
        components/LayoutProvider/handlers/fieldDuplicatedHandler.es.js|55 col 16 error| 'getLabel' was used before it was defined.  no-use-before-define
        components/LayoutProvider/handlers/fieldDuplicatedHandler.es.js|111 col 3 error| 'getValidation' was used before it was defined.  no-use-before-define
        components/LayoutProvider/util/fields.es.js|74 col 9 error| 'getFieldProperty' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|88 col 34 error| 'getFieldType' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|95 col 5 error| 'fieldWithOptions' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|99 col 21 error| 'getFieldOptions' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|102 col 17 error| 'optionBelongsToRule' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|132 col 5 error| 'fieldWithOptions' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|149 col 13 error| 'syncActions' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|168 col 30 error| 'getExpressionFields' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|171 col 36 error| 'getExpressionFields' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|216 col 9 error| 'findRuleByFieldName' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|260 col 23 error| 'targetFieldExists' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|264 col 23 error| 'targetFieldExists' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|272 col 11 error| 'targetFieldExists' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|283 col 9 error| 'targetFieldExists' was used before it was defined.  no-use-before-define
        components/RuleBuilder/RulesSupport.es.js|294 col 13 error| 'targetFieldExists' was used before it was defined.  no-use-before-define
        util/fieldSupport.es.js|50 col 38 error| 'getDefaultFieldName' was used before it was defined.  no-use-before-define
        util/fieldSupport.es.js|61 col 11 error| 'normalizeSettingsContextPages' was used before it was defined.  no-use-before-define
        util/fieldSupport.es.js|75 col 6 error| 'getFieldProperties' was used before it was defined.  no-use-before-define
        util/fieldSupport.es.js|82 col 15 error| 'generateInstanceId' was used before it was defined.  no-use-before-define
        util/fieldSupport.es.js|196 col 7 error| 'isFieldSet' was used before it was defined.  no-use-before-define

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleBuilder/RulesSupport.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleBuilder/RulesSupport.es.js
@@ -59,6 +59,107 @@ const clearAllConditionFieldValues = (condition) => {
 	return condition;
 };
 
+const fieldWithOptions = (fieldType) => {
+	return (
+		fieldType === 'radio' ||
+		fieldType === 'checkbox_multiple' ||
+		fieldType === 'select'
+	);
+};
+
+const getExpressionFields = (action, regex = DEFAULT_FIELD_NAME_REGEX) => {
+	return action.expression.match(regex);
+};
+
+const getFieldOptions = (fieldName, pages) => {
+	let options = [];
+	const visitor = new PagesVisitor(pages);
+
+	const field = visitor.findField((field) => {
+		return field.fieldName === fieldName;
+	});
+
+	options = field ? field.options : [];
+
+	return options;
+};
+
+const getFieldType = (fieldName, pages) => {
+	return getFieldProperty(pages, fieldName, 'type');
+};
+
+const optionBelongsToRule = (condition, options) => {
+	return options.some(
+		(option) => option.value === condition.operands[1].value
+	);
+};
+
+const targetFieldExists = (target, pages) => {
+	const visitor = new PagesVisitor(pages);
+
+	let targetFieldExists = false;
+
+	visitor.mapFields(
+		({fieldName}) => {
+			if (target === fieldName) {
+				targetFieldExists = true;
+			}
+		},
+		true,
+		true
+	);
+
+	return targetFieldExists;
+};
+
+const syncActions = (pages, actions) => {
+	actions.forEach((action) => {
+		if (action.action === 'auto-fill') {
+			const {inputs, outputs} = action;
+
+			Object.keys(inputs)
+				.filter((key) => !targetFieldExists(inputs[key], pages))
+				.map((key) => delete inputs[key]);
+
+			Object.keys(outputs)
+				.filter((key) => !targetFieldExists(outputs[key], pages))
+				.map((key) => delete outputs[key]);
+		}
+		else if (action.action === 'calculate') {
+			const expressionFields = getExpressionFields(action);
+
+			if (expressionFields && expressionFields.length > 0) {
+				expressionFields.forEach((field) => {
+					if (!targetFieldExists(field, pages)) {
+						const inexistentField = new RegExp(field, 'g');
+
+						action.expression = action.expression.replace(
+							inexistentField,
+							''
+						);
+					}
+				});
+			}
+
+			if (!targetFieldExists(action.target, pages)) {
+				action.target = '';
+			}
+		}
+		else if (action.action === 'jump-to-page') {
+			const target = parseInt(action.target, 10) + 1;
+
+			if (pages.length < 3 || target > pages.length) {
+				action.target = '';
+			}
+		}
+		else if (!targetFieldExists(action.target, pages)) {
+			action.target = '';
+		}
+	});
+
+	return actions;
+};
+
 const formatRules = (pages, rules) => {
 	const visitor = new PagesVisitor(pages);
 
@@ -204,18 +305,6 @@ const fieldNameBelongsToCondition = (fieldName, conditions) => {
 		.some((fieldFound) => fieldFound === true);
 };
 
-const fieldWithOptions = (fieldType) => {
-	return (
-		fieldType === 'radio' ||
-		fieldType === 'checkbox_multiple' ||
-		fieldType === 'select'
-	);
-};
-
-const findInvalidRule = (rule) => {
-	return findRuleByFieldName('', [rule]);
-};
-
 const findRuleByFieldName = (fieldName, rules) => {
 	return rules.some(
 		(rule) =>
@@ -224,97 +313,8 @@ const findRuleByFieldName = (fieldName, rules) => {
 	);
 };
 
-const getExpressionFields = (action, regex = DEFAULT_FIELD_NAME_REGEX) => {
-	return action.expression.match(regex);
-};
-
-const getFieldOptions = (fieldName, pages) => {
-	let options = [];
-	const visitor = new PagesVisitor(pages);
-
-	const field = visitor.findField((field) => {
-		return field.fieldName === fieldName;
-	});
-
-	options = field ? field.options : [];
-
-	return options;
-};
-
-const getFieldType = (fieldName, pages) => {
-	return getFieldProperty(pages, fieldName, 'type');
-};
-
-const optionBelongsToRule = (condition, options) => {
-	return options.some(
-		(option) => option.value === condition.operands[1].value
-	);
-};
-
-const syncActions = (pages, actions) => {
-	actions.forEach((action) => {
-		if (action.action === 'auto-fill') {
-			const {inputs, outputs} = action;
-
-			Object.keys(inputs)
-				.filter((key) => !targetFieldExists(inputs[key], pages))
-				.map((key) => delete inputs[key]);
-
-			Object.keys(outputs)
-				.filter((key) => !targetFieldExists(outputs[key], pages))
-				.map((key) => delete outputs[key]);
-		}
-		else if (action.action === 'calculate') {
-			const expressionFields = getExpressionFields(action);
-
-			if (expressionFields && expressionFields.length > 0) {
-				expressionFields.forEach((field) => {
-					if (!targetFieldExists(field, pages)) {
-						const inexistentField = new RegExp(field, 'g');
-
-						action.expression = action.expression.replace(
-							inexistentField,
-							''
-						);
-					}
-				});
-			}
-
-			if (!targetFieldExists(action.target, pages)) {
-				action.target = '';
-			}
-		}
-		else if (action.action === 'jump-to-page') {
-			const target = parseInt(action.target, 10) + 1;
-
-			if (pages.length < 3 || target > pages.length) {
-				action.target = '';
-			}
-		}
-		else if (!targetFieldExists(action.target, pages)) {
-			action.target = '';
-		}
-	});
-
-	return actions;
-};
-
-const targetFieldExists = (target, pages) => {
-	const visitor = new PagesVisitor(pages);
-
-	let targetFieldExists = false;
-
-	visitor.mapFields(
-		({fieldName}) => {
-			if (target === fieldName) {
-				targetFieldExists = true;
-			}
-		},
-		true,
-		true
-	);
-
-	return targetFieldExists;
+const findInvalidRule = (rule) => {
+	return findRuleByFieldName('', [rule]);
 };
 
 export default {
