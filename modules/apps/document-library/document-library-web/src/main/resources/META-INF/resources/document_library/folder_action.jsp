liferay-frontend/liferay-portal

commit 0d183ba1f4f07c8df7182d7a4319f7d6b3d0e61a

LPS-102111 Document Library info-panel replace `sidebar-actions` with autofit pattern in `sidebar-header`.

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/folder_action.jsp
@@ -21,122 +21,124 @@ FolderActionDisplayContext folderActionDisplayContext = new FolderActionDisplayC
 %>
 
 <c:if test="<%= folderActionDisplayContext.isShowActions() %>">
-	<liferay-ui:icon-menu
-		direction="left-side"
-		icon="<%= StringPool.BLANK %>"
-		markupView="lexicon"
-		message='<%= LanguageUtil.get(request, "actions") %>'
-		showWhenSingleIcon="<%= true %>"
-	>
-		<c:if test="<%= folderActionDisplayContext.isDownloadFolderActionVisible() %>">
-			<liferay-ui:icon
-				message="download"
-				method="get"
-				url="<%= folderActionDisplayContext.getDownloadFolderURL() %>"
-			/>
-		</c:if>
-
-		<c:if test="<%= folderActionDisplayContext.isEditFolderActionVisible() %>">
-			<liferay-ui:icon
-				message="edit"
-				url="<%= folderActionDisplayContext.getEditFolderURL() %>"
-			/>
-		</c:if>
-
-		<c:if test="<%= folderActionDisplayContext.isMoveFolderActionVisible() %>">
-			<liferay-ui:icon
-				message="move"
-				url="<%= folderActionDisplayContext.getMoveFolderURL() %>"
-			/>
-		</c:if>
-
-		<c:if test="<%= folderActionDisplayContext.isDeleteExpiredTemporaryFileEntriesActionVisible() %>">
-			<liferay-ui:icon
-				message="delete-expired-temporary-files"
-				url="<%= folderActionDisplayContext.getDeleteExpiredTemporaryFileEntriesURL() %>"
-			/>
-		</c:if>
-
-		<c:if test="<%= folderActionDisplayContext.isAddFolderActionVisible() %>">
-			<liferay-ui:icon
-				message="add-folder"
-				url="<%= folderActionDisplayContext.getAddFolderURL() %>"
-			/>
-		</c:if>
-
-		<c:if test="<%= folderActionDisplayContext.isAddRepositoryActionVisible() %>">
-			<liferay-ui:icon
-				message="add-repository"
-				url="<%= folderActionDisplayContext.getAddRepositoryURL() %>"
-			/>
-		</c:if>
-
-		<c:if test="<%= folderActionDisplayContext.isAddMediaActionVisible() %>">
-			<liferay-ui:icon
-				message="add-file-entry"
-				url="<%= folderActionDisplayContext.getAddMediaURL() %>"
-			/>
-
-			<c:if test="<%= folderActionDisplayContext.isMultipleUploadSupported() %>">
+	<li class="autofit-col">
+		<liferay-ui:icon-menu
+			direction="left-side"
+			icon="<%= StringPool.BLANK %>"
+			markupView="lexicon"
+			message='<%= LanguageUtil.get(request, "actions") %>'
+			showWhenSingleIcon="<%= true %>"
+		>
+			<c:if test="<%= folderActionDisplayContext.isDownloadFolderActionVisible() %>">
 				<liferay-ui:icon
-					cssClass="hide upload-multiple-documents"
-					message="multiple-media"
-					url="<%= folderActionDisplayContext.getAddMultipleMediaURL() %>"
+					message="download"
+					method="get"
+					url="<%= folderActionDisplayContext.getDownloadFolderURL() %>"
 				/>
 			</c:if>
-		</c:if>
 
-		<c:if test="<%= folderActionDisplayContext.isViewSlideShowActionVisible() %>">
-			<liferay-ui:icon
-				cssClass='<%= folderActionDisplayContext.getRandomNamespace() + "-slide-show" %>'
-				message="view-slide-show"
-				url="javascript:;"
-			/>
-		</c:if>
+			<c:if test="<%= folderActionDisplayContext.isEditFolderActionVisible() %>">
+				<liferay-ui:icon
+					message="edit"
+					url="<%= folderActionDisplayContext.getEditFolderURL() %>"
+				/>
+			</c:if>
 
-		<c:if test="<%= folderActionDisplayContext.isAddFileShortcutActionVisible() %>">
-			<liferay-ui:icon
-				message="add-shortcut"
-				url="<%= folderActionDisplayContext.getAddFileShortcutURL() %>"
-			/>
-		</c:if>
+			<c:if test="<%= folderActionDisplayContext.isMoveFolderActionVisible() %>">
+				<liferay-ui:icon
+					message="move"
+					url="<%= folderActionDisplayContext.getMoveFolderURL() %>"
+				/>
+			</c:if>
 
-		<c:if test="<%= folderActionDisplayContext.isAccessFromDesktopActionVisible() %>">
-			<liferay-util:include page="/document_library/access_from_desktop.jsp" servletContext="<%= application %>" />
-		</c:if>
+			<c:if test="<%= folderActionDisplayContext.isDeleteExpiredTemporaryFileEntriesActionVisible() %>">
+				<liferay-ui:icon
+					message="delete-expired-temporary-files"
+					url="<%= folderActionDisplayContext.getDeleteExpiredTemporaryFileEntriesURL() %>"
+				/>
+			</c:if>
 
-		<c:if test="<%= folderActionDisplayContext.isPermissionsActionVisible() %>">
-			<liferay-security:permissionsURL
-				modelResource="<%= folderActionDisplayContext.getModelResource() %>"
-				modelResourceDescription="<%= HtmlUtil.escape(folderActionDisplayContext.getModelResourceDescription()) %>"
-				resourcePrimKey="<%= String.valueOf(folderActionDisplayContext.getResourcePrimKey()) %>"
-				var="permissionsURL"
-				windowState="<%= LiferayWindowState.POP_UP.toString() %>"
-			/>
+			<c:if test="<%= folderActionDisplayContext.isAddFolderActionVisible() %>">
+				<liferay-ui:icon
+					message="add-folder"
+					url="<%= folderActionDisplayContext.getAddFolderURL() %>"
+				/>
+			</c:if>
 
-			<liferay-ui:icon
-				message="permissions"
-				method="get"
-				url="<%= permissionsURL %>"
-				useDialog="<%= true %>"
-			/>
-		</c:if>
+			<c:if test="<%= folderActionDisplayContext.isAddRepositoryActionVisible() %>">
+				<liferay-ui:icon
+					message="add-repository"
+					url="<%= folderActionDisplayContext.getAddRepositoryURL() %>"
+				/>
+			</c:if>
 
-		<c:if test="<%= folderActionDisplayContext.isDeleteFolderActionVisible() %>">
-			<liferay-ui:icon-delete
-				trash="<%= folderActionDisplayContext.isTrashEnabled() %>"
-				url="<%= folderActionDisplayContext.getDeleteFolderURL() %>"
-			/>
-		</c:if>
+			<c:if test="<%= folderActionDisplayContext.isAddMediaActionVisible() %>">
+				<liferay-ui:icon
+					message="add-file-entry"
+					url="<%= folderActionDisplayContext.getAddMediaURL() %>"
+				/>
 
-		<c:if test="<%= folderActionDisplayContext.isPublishFolderActionVisible() %>">
-			<liferay-ui:icon-delete
-				confirmation="are-you-sure-you-want-to-publish-the-selected-folder"
-				message="publish-to-live"
-				url="<%= folderActionDisplayContext.getPublishFolderURL() %>"
-			/>
-		</c:if>
-	</liferay-ui:icon-menu>
+				<c:if test="<%= folderActionDisplayContext.isMultipleUploadSupported() %>">
+					<liferay-ui:icon
+						cssClass="hide upload-multiple-documents"
+						message="multiple-media"
+						url="<%= folderActionDisplayContext.getAddMultipleMediaURL() %>"
+					/>
+				</c:if>
+			</c:if>
+
+			<c:if test="<%= folderActionDisplayContext.isViewSlideShowActionVisible() %>">
+				<liferay-ui:icon
+					cssClass='<%= folderActionDisplayContext.getRandomNamespace() + "-slide-show" %>'
+					message="view-slide-show"
+					url="javascript:;"
+				/>
+			</c:if>
+
+			<c:if test="<%= folderActionDisplayContext.isAddFileShortcutActionVisible() %>">
+				<liferay-ui:icon
+					message="add-shortcut"
+					url="<%= folderActionDisplayContext.getAddFileShortcutURL() %>"
+				/>
+			</c:if>
+
+			<c:if test="<%= folderActionDisplayContext.isAccessFromDesktopActionVisible() %>">
+				<liferay-util:include page="/document_library/access_from_desktop.jsp" servletContext="<%= application %>" />
+			</c:if>
+
+			<c:if test="<%= folderActionDisplayContext.isPermissionsActionVisible() %>">
+				<liferay-security:permissionsURL
+					modelResource="<%= folderActionDisplayContext.getModelResource() %>"
+					modelResourceDescription="<%= HtmlUtil.escape(folderActionDisplayContext.getModelResourceDescription()) %>"
+					resourcePrimKey="<%= String.valueOf(folderActionDisplayContext.getResourcePrimKey()) %>"
+					var="permissionsURL"
+					windowState="<%= LiferayWindowState.POP_UP.toString() %>"
+				/>
+
+				<liferay-ui:icon
+					message="permissions"
+					method="get"
+					url="<%= permissionsURL %>"
+					useDialog="<%= true %>"
+				/>
+			</c:if>
+
+			<c:if test="<%= folderActionDisplayContext.isDeleteFolderActionVisible() %>">
+				<liferay-ui:icon-delete
+					trash="<%= folderActionDisplayContext.isTrashEnabled() %>"
+					url="<%= folderActionDisplayContext.getDeleteFolderURL() %>"
+				/>
+			</c:if>
+
+			<c:if test="<%= folderActionDisplayContext.isPublishFolderActionVisible() %>">
+				<liferay-ui:icon-delete
+					confirmation="are-you-sure-you-want-to-publish-the-selected-folder"
+					message="publish-to-live"
+					url="<%= folderActionDisplayContext.getPublishFolderURL() %>"
+				/>
+			</c:if>
+		</liferay-ui:icon-menu>
+	</li>
 
 	<aui:script use="uploader">
 		if (!A.UA.ios && A.Uploader.TYPE != 'none') {

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/info_panel.jsp
@@ -65,9 +65,8 @@ if (ListUtil.isEmpty(folders) && ListUtil.isEmpty(fileEntries) && ListUtil.isEmp
 						<li class="autofit-col">
 							<liferay-util:include page="/document_library/subscribe.jsp" servletContext="<%= application %>" />
 						</li>
-						<li class="autofit-col">
-							<liferay-util:include page="/document_library/folder_action.jsp" servletContext="<%= application %>" />
-						</li>
+
+						<liferay-util:include page="/document_library/folder_action.jsp" servletContext="<%= application %>" />
 					</ul>
 				</div>
 			</div>

commit 34789119f9ccd6352b6d6c97c3ecd5280495d702

LPS-102111 document-library-web fix broken folder cards caused by 0d183ba. Update info panel markup for individual documents

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/css/main.scss
@@ -550,6 +550,18 @@ $productMenuWidth: 320px;
 	}
 
 	.sidebar-body {
+		.collaborators {
+			.component-title {
+				font-weight: 600;
+				margin: 0;
+			}
+
+			.component-subtitle {
+				font-size: 14px;
+				font-weight: 600;
+			}
+		}
+
 		.thumbnail {
 			margin-bottom: 1.5rem;
 		}

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/folder_action.jsp
@@ -21,124 +21,122 @@ FolderActionDisplayContext folderActionDisplayContext = new FolderActionDisplayC
 %>
 
 <c:if test="<%= folderActionDisplayContext.isShowActions() %>">
-	<li class="autofit-col">
-		<liferay-ui:icon-menu
-			direction="left-side"
-			icon="<%= StringPool.BLANK %>"
-			markupView="lexicon"
-			message='<%= LanguageUtil.get(request, "actions") %>'
-			showWhenSingleIcon="<%= true %>"
-		>
-			<c:if test="<%= folderActionDisplayContext.isDownloadFolderActionVisible() %>">
+	<liferay-ui:icon-menu
+		direction="left-side"
+		icon="<%= StringPool.BLANK %>"
+		markupView="lexicon"
+		message='<%= LanguageUtil.get(request, "actions") %>'
+		showWhenSingleIcon="<%= true %>"
+	>
+		<c:if test="<%= folderActionDisplayContext.isDownloadFolderActionVisible() %>">
+			<liferay-ui:icon
+				message="download"
+				method="get"
+				url="<%= folderActionDisplayContext.getDownloadFolderURL() %>"
+			/>
+		</c:if>
+
+		<c:if test="<%= folderActionDisplayContext.isEditFolderActionVisible() %>">
+			<liferay-ui:icon
+				message="edit"
+				url="<%= folderActionDisplayContext.getEditFolderURL() %>"
+			/>
+		</c:if>
+
+		<c:if test="<%= folderActionDisplayContext.isMoveFolderActionVisible() %>">
+			<liferay-ui:icon
+				message="move"
+				url="<%= folderActionDisplayContext.getMoveFolderURL() %>"
+			/>
+		</c:if>
+
+		<c:if test="<%= folderActionDisplayContext.isDeleteExpiredTemporaryFileEntriesActionVisible() %>">
+			<liferay-ui:icon
+				message="delete-expired-temporary-files"
+				url="<%= folderActionDisplayContext.getDeleteExpiredTemporaryFileEntriesURL() %>"
+			/>
+		</c:if>
+
+		<c:if test="<%= folderActionDisplayContext.isAddFolderActionVisible() %>">
+			<liferay-ui:icon
+				message="add-folder"
+				url="<%= folderActionDisplayContext.getAddFolderURL() %>"
+			/>
+		</c:if>
+
+		<c:if test="<%= folderActionDisplayContext.isAddRepositoryActionVisible() %>">
+			<liferay-ui:icon
+				message="add-repository"
+				url="<%= folderActionDisplayContext.getAddRepositoryURL() %>"
+			/>
+		</c:if>
+
+		<c:if test="<%= folderActionDisplayContext.isAddMediaActionVisible() %>">
+			<liferay-ui:icon
+				message="add-file-entry"
+				url="<%= folderActionDisplayContext.getAddMediaURL() %>"
+			/>
+
+			<c:if test="<%= folderActionDisplayContext.isMultipleUploadSupported() %>">
 				<liferay-ui:icon
-					message="download"
-					method="get"
-					url="<%= folderActionDisplayContext.getDownloadFolderURL() %>"
+					cssClass="hide upload-multiple-documents"
+					message="multiple-media"
+					url="<%= folderActionDisplayContext.getAddMultipleMediaURL() %>"
 				/>
 			</c:if>
+		</c:if>
 
-			<c:if test="<%= folderActionDisplayContext.isEditFolderActionVisible() %>">
-				<liferay-ui:icon
-					message="edit"
-					url="<%= folderActionDisplayContext.getEditFolderURL() %>"
-				/>
-			</c:if>
+		<c:if test="<%= folderActionDisplayContext.isViewSlideShowActionVisible() %>">
+			<liferay-ui:icon
+				cssClass='<%= folderActionDisplayContext.getRandomNamespace() + "-slide-show" %>'
+				message="view-slide-show"
+				url="javascript:;"
+			/>
+		</c:if>
 
-			<c:if test="<%= folderActionDisplayContext.isMoveFolderActionVisible() %>">
-				<liferay-ui:icon
-					message="move"
-					url="<%= folderActionDisplayContext.getMoveFolderURL() %>"
-				/>
-			</c:if>
+		<c:if test="<%= folderActionDisplayContext.isAddFileShortcutActionVisible() %>">
+			<liferay-ui:icon
+				message="add-shortcut"
+				url="<%= folderActionDisplayContext.getAddFileShortcutURL() %>"
+			/>
+		</c:if>
 
-			<c:if test="<%= folderActionDisplayContext.isDeleteExpiredTemporaryFileEntriesActionVisible() %>">
-				<liferay-ui:icon
-					message="delete-expired-temporary-files"
-					url="<%= folderActionDisplayContext.getDeleteExpiredTemporaryFileEntriesURL() %>"
-				/>
-			</c:if>
+		<c:if test="<%= folderActionDisplayContext.isAccessFromDesktopActionVisible() %>">
+			<liferay-util:include page="/document_library/access_from_desktop.jsp" servletContext="<%= application %>" />
+		</c:if>
 
-			<c:if test="<%= folderActionDisplayContext.isAddFolderActionVisible() %>">
-				<liferay-ui:icon
-					message="add-folder"
-					url="<%= folderActionDisplayContext.getAddFolderURL() %>"
-				/>
-			</c:if>
+		<c:if test="<%= folderActionDisplayContext.isPermissionsActionVisible() %>">
+			<liferay-security:permissionsURL
+				modelResource="<%= folderActionDisplayContext.getModelResource() %>"
+				modelResourceDescription="<%= HtmlUtil.escape(folderActionDisplayContext.getModelResourceDescription()) %>"
+				resourcePrimKey="<%= String.valueOf(folderActionDisplayContext.getResourcePrimKey()) %>"
+				var="permissionsURL"
+				windowState="<%= LiferayWindowState.POP_UP.toString() %>"
+			/>
 
-			<c:if test="<%= folderActionDisplayContext.isAddRepositoryActionVisible() %>">
-				<liferay-ui:icon
-					message="add-repository"
-					url="<%= folderActionDisplayContext.getAddRepositoryURL() %>"
-				/>
-			</c:if>
+			<liferay-ui:icon
+				message="permissions"
+				method="get"
+				url="<%= permissionsURL %>"
+				useDialog="<%= true %>"
+			/>
+		</c:if>
 
-			<c:if test="<%= folderActionDisplayContext.isAddMediaActionVisible() %>">
-				<liferay-ui:icon
-					message="add-file-entry"
-					url="<%= folderActionDisplayContext.getAddMediaURL() %>"
-				/>
+		<c:if test="<%= folderActionDisplayContext.isDeleteFolderActionVisible() %>">
+			<liferay-ui:icon-delete
+				trash="<%= folderActionDisplayContext.isTrashEnabled() %>"
+				url="<%= folderActionDisplayContext.getDeleteFolderURL() %>"
+			/>
+		</c:if>
 
-				<c:if test="<%= folderActionDisplayContext.isMultipleUploadSupported() %>">
-					<liferay-ui:icon
-						cssClass="hide upload-multiple-documents"
-						message="multiple-media"
-						url="<%= folderActionDisplayContext.getAddMultipleMediaURL() %>"
-					/>
-				</c:if>
-			</c:if>
-
-			<c:if test="<%= folderActionDisplayContext.isViewSlideShowActionVisible() %>">
-				<liferay-ui:icon
-					cssClass='<%= folderActionDisplayContext.getRandomNamespace() + "-slide-show" %>'
-					message="view-slide-show"
-					url="javascript:;"
-				/>
-			</c:if>
-
-			<c:if test="<%= folderActionDisplayContext.isAddFileShortcutActionVisible() %>">
-				<liferay-ui:icon
-					message="add-shortcut"
-					url="<%= folderActionDisplayContext.getAddFileShortcutURL() %>"
-				/>
-			</c:if>
-
-			<c:if test="<%= folderActionDisplayContext.isAccessFromDesktopActionVisible() %>">
-				<liferay-util:include page="/document_library/access_from_desktop.jsp" servletContext="<%= application %>" />
-			</c:if>
-
-			<c:if test="<%= folderActionDisplayContext.isPermissionsActionVisible() %>">
-				<liferay-security:permissionsURL
-					modelResource="<%= folderActionDisplayContext.getModelResource() %>"
-					modelResourceDescription="<%= HtmlUtil.escape(folderActionDisplayContext.getModelResourceDescription()) %>"
-					resourcePrimKey="<%= String.valueOf(folderActionDisplayContext.getResourcePrimKey()) %>"
-					var="permissionsURL"
-					windowState="<%= LiferayWindowState.POP_UP.toString() %>"
-				/>
-
-				<liferay-ui:icon
-					message="permissions"
-					method="get"
-					url="<%= permissionsURL %>"
-					useDialog="<%= true %>"
-				/>
-			</c:if>
-
-			<c:if test="<%= folderActionDisplayContext.isDeleteFolderActionVisible() %>">
-				<liferay-ui:icon-delete
-					trash="<%= folderActionDisplayContext.isTrashEnabled() %>"
-					url="<%= folderActionDisplayContext.getDeleteFolderURL() %>"
-				/>
-			</c:if>
-
-			<c:if test="<%= folderActionDisplayContext.isPublishFolderActionVisible() %>">
-				<liferay-ui:icon-delete
-					confirmation="are-you-sure-you-want-to-publish-the-selected-folder"
-					message="publish-to-live"
-					url="<%= folderActionDisplayContext.getPublishFolderURL() %>"
-				/>
-			</c:if>
-		</liferay-ui:icon-menu>
-	</li>
+		<c:if test="<%= folderActionDisplayContext.isPublishFolderActionVisible() %>">
+			<liferay-ui:icon-delete
+				confirmation="are-you-sure-you-want-to-publish-the-selected-folder"
+				message="publish-to-live"
+				url="<%= folderActionDisplayContext.getPublishFolderURL() %>"
+			/>
+		</c:if>
+	</liferay-ui:icon-menu>
 
 	<aui:script use="uploader">
 		if (!A.UA.ios && A.Uploader.TYPE != 'none') {

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/info_panel.jsp
@@ -66,7 +66,15 @@ if (ListUtil.isEmpty(folders) && ListUtil.isEmpty(fileEntries) && ListUtil.isEmp
 							<liferay-util:include page="/document_library/subscribe.jsp" servletContext="<%= application %>" />
 						</li>
 
-						<liferay-util:include page="/document_library/folder_action.jsp" servletContext="<%= application %>" />
+						<%
+						FolderActionDisplayContext folderActionDisplayContext = new FolderActionDisplayContext(dlTrashHelper, request, liferayPortletResponse);
+						%>
+
+						<c:if test="<%= folderActionDisplayContext.isShowActions() %>">
+							<li class="autofit-col">
+								<liferay-util:include page="/document_library/folder_action.jsp" servletContext="<%= application %>" />
+							</li>
+						</c:if>
 					</ul>
 				</div>
 			</div>

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/info_panel_file_entry.jsp
@@ -34,26 +34,28 @@ long assetClassPK = DLAssetHelperUtil.getAssetClassPK(fileEntry, fileVersion);
 		<clay:content-col
 			expand="<%= true %>"
 		>
-			<h1 class="component-title">
-				<%= HtmlUtil.escape(fileVersion.getTitle()) %>
-			</h1>
+			<clay:content-section>
+				<h1 class="component-title">
+					<%= HtmlUtil.escape(fileVersion.getTitle()) %>
+				</h1>
 
-			<c:if test="<%= !Objects.equals(fileVersion.getTitle(), fileVersion.getFileName()) %>">
-				<h3 class="component-title">
-					<%= HtmlUtil.escape(fileVersion.getFileName()) %>
-				</h3>
-			</c:if>
+				<c:if test="<%= !Objects.equals(fileVersion.getTitle(), fileVersion.getFileName()) %>">
+					<p class="component-subtitle">
+						<%= HtmlUtil.escape(fileVersion.getFileName()) %>
+					</p>
+				</c:if>
 
-			<c:if test="<%= dlViewFileVersionDisplayContext.isVersionInfoVisible() %>">
-				<clay:label
-					displayType="info"
-					label='<%= LanguageUtil.get(request, "version") + StringPool.SPACE + fileVersion.getVersion() %>'
-				/>
-			</c:if>
+				<c:if test="<%= dlViewFileVersionDisplayContext.isVersionInfoVisible() %>">
+					<clay:label
+						displayType="info"
+						label='<%= LanguageUtil.get(request, "version") + StringPool.SPACE + fileVersion.getVersion() %>'
+					/>
+				</c:if>
 
-			<aui:model-context bean="<%= fileVersion %>" model="<%= DLFileVersion.class %>" />
+				<aui:model-context bean="<%= fileVersion %>" model="<%= DLFileVersion.class %>" />
 
-			<aui:workflow-status model="<%= DLFileEntry.class %>" showIcon="<%= false %>" showLabel="<%= false %>" status="<%= fileVersion.getStatus() %>" />
+				<aui:workflow-status model="<%= DLFileEntry.class %>" showIcon="<%= false %>" showLabel="<%= false %>" status="<%= fileVersion.getStatus() %>" />
+			</clay:content-section>
 		</clay:content-col>
 
 		<c:if test="<%= !hideActions %>">
@@ -99,16 +101,15 @@ long assetClassPK = DLAssetHelperUtil.getAssetClassPK(fileEntry, fileVersion);
 				name="com.liferay.document.library.web#/document_library/info_panel_file_entry.jsp#fileEntryOwner"
 			>
 				<clay:content-row
-					cssClass="sidebar-panel widget-metadata"
+					cssClass="sidebar-section widget-metadata"
+					noGutters="true"
 				>
 
 					<%
 					User owner = UserLocalServiceUtil.fetchUser(fileEntry.getUserId());
 					%>
 
-					<clay:content-col
-						cssClass="inline-item-before"
-					>
+					<clay:content-col>
 						<liferay-ui:user-portrait
 							user="<%= owner %>"
 						/>
@@ -117,28 +118,22 @@ long assetClassPK = DLAssetHelperUtil.getAssetClassPK(fileEntry, fileVersion);
 					<clay:content-col
 						expand="<%= true %>"
 					>
-						<clay:content-row>
-							<clay:content-col
-								expand="<%= true %>"
-							>
-								<div class="component-title h4 username">
-									<c:if test="<%= owner != null %>">
-										<a href="<%= owner.isDefaultUser() ? StringPool.BLANK : owner.getDisplayURL(themeDisplay) %>"><%= HtmlUtil.escape(owner.getFullName()) %></a>
-									</c:if>
-								</div>
+						<div class="component-title username">
+							<c:if test="<%= owner != null %>">
+								<a href="<%= owner.isDefaultUser() ? StringPool.BLANK : owner.getDisplayURL(themeDisplay) %>"><%= HtmlUtil.escape(owner.getFullName()) %></a>
+							</c:if>
+						</div>
 
-								<small class="text-muted">
-									<liferay-ui:message key="owner" />
-								</small>
-							</clay:content-col>
-						</clay:content-row>
+						<div class="component-subtitle">
+							<liferay-ui:message key="owner" />
+						</div>
 					</clay:content-col>
 				</clay:content-row>
 			</liferay-dynamic-section:dynamic-section>
 
 			<c:if test="<%= dlViewFileVersionDisplayContext.isDownloadLinkVisible() || dlViewFileVersionDisplayContext.isSharingLinkVisible() %>">
 				<div class="sidebar-section">
-					<div class="btn-group sidebar-panel">
+					<div class="btn-group">
 						<c:if test="<%= dlViewFileVersionDisplayContext.isDownloadLinkVisible() %>">
 							<c:choose>
 								<c:when test="<%= PropsValues.DL_FILE_ENTRY_CONVERSIONS_ENABLED && DocumentConversionUtil.isEnabled() %>">
@@ -232,36 +227,76 @@ long assetClassPK = DLAssetHelperUtil.getAssetClassPK(fileEntry, fileVersion);
 							</div>
 						</c:if>
 					</div>
+				</div>
 
-					<div class="sidebar-panel">
+				<div class="sidebar-section">
+
+					<%
+					boolean isLatestVersion = fileVersion.equals(fileEntry.getLatestFileVersion());
+
+					String urlLabel = null;
+
+					if (isLatestVersion) {
+						urlLabel = LanguageUtil.get(resourceBundle, "latest-version-url");
+					}
+					else {
+						urlLabel = LanguageUtil.format(request, "version-x-url", fileVersion.getVersion());
+					}
+
+					String urlInputId = liferayPortletResponse.getNamespace() + "urlInput";
+					%>
+
+					<div class="form-group">
+						<label for="<%= urlInputId %>"><%= urlLabel %></label>
+
+						<div class="input-group input-group-sm">
+							<div class="input-group-item input-group-prepend">
+								<input class="form-control" id="<%= urlInputId %>" value="<%= DLURLHelperUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, StringPool.BLANK, !isLatestVersion, true) %>" />
+							</div>
+
+							<span class="input-group-append input-group-item input-group-item-shrink">
+								<clay:button
+									cssClass="dm-infopanel-copy-clipboard lfr-portal-tooltip"
+									data-clipboard-target='<%= "#" + urlInputId %>'
+									displayType="secondary"
+									icon="paste"
+									title="copy-link"
+								/>
+							</span>
+						</div>
+					</div>
+
+					<c:if test="<%= portletDisplay.isWebDAVEnabled() && fileEntry.isSupportsSocial() && isLatestVersion %>">
 
 						<%
-						boolean isLatestVersion = fileVersion.equals(fileEntry.getLatestFileVersion());
+						String webDavHelpMessage = null;
 
-						String urlLabel = null;
-
-						if (isLatestVersion) {
-							urlLabel = LanguageUtil.get(resourceBundle, "latest-version-url");
+						if (BrowserSnifferUtil.isWindows(request)) {
+							webDavHelpMessage = LanguageUtil.format(request, "webdav-windows-help", new Object[] {"https://support.microsoft.com/en-us/kb/892211", "https://help.liferay.com/hc/en-us/articles/360028720352-Desktop-Access-to-Documents-and-Media"}, false);
 						}
 						else {
-							urlLabel = LanguageUtil.format(request, "version-x-url", fileVersion.getVersion());
+							webDavHelpMessage = LanguageUtil.format(request, "webdav-help", "https://help.liferay.com/hc/en-us/articles/360028720352-Desktop-Access-to-Documents-and-Media", false);
 						}
 
-						String urlInputId = liferayPortletResponse.getNamespace() + "urlInput";
+						String webDavURLInputId = liferayPortletResponse.getNamespace() + "webDavURLInput";
 						%>
 
 						<div class="form-group">
-							<label for="<%= urlInputId %>"><%= urlLabel %></label>
+							<label for="<%= webDavURLInputId %>">
+								<liferay-ui:message key='<%= TextFormatter.format("webDavURL", TextFormatter.K) %>' />
+
+								<liferay-ui:icon-help message="<%= webDavHelpMessage %>" />
+							</label>
 
 							<div class="input-group input-group-sm">
 								<div class="input-group-item input-group-prepend">
-									<input class="form-control" id="<%= urlInputId %>" value="<%= DLURLHelperUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, StringPool.BLANK, !isLatestVersion, true) %>" />
+									<input class="form-control" id="<%= webDavURLInputId %>" value="<%= DLURLHelperUtil.getWebDavURL(themeDisplay, fileEntry.getFolder(), fileEntry) %>" />
 								</div>
 
 								<span class="input-group-append input-group-item input-group-item-shrink">
 									<clay:button
 										cssClass="dm-infopanel-copy-clipboard lfr-portal-tooltip"
-										data-clipboard-target='<%= "#" + urlInputId %>'
+										data-clipboard-target='<%= "#" + webDavURLInputId %>'
 										displayType="secondary"
 										icon="paste"
 										title="copy-link"
@@ -269,47 +304,7 @@ long assetClassPK = DLAssetHelperUtil.getAssetClassPK(fileEntry, fileVersion);
 								</span>
 							</div>
 						</div>
-
-						<c:if test="<%= portletDisplay.isWebDAVEnabled() && fileEntry.isSupportsSocial() && isLatestVersion %>">
-
-							<%
-							String webDavHelpMessage = null;
-
-							if (BrowserSnifferUtil.isWindows(request)) {
-								webDavHelpMessage = LanguageUtil.format(request, "webdav-windows-help", new Object[] {"https://support.microsoft.com/en-us/kb/892211", "https://help.liferay.com/hc/en-us/articles/360028720352-Desktop-Access-to-Documents-and-Media"}, false);
-							}
-							else {
-								webDavHelpMessage = LanguageUtil.format(request, "webdav-help", "https://help.liferay.com/hc/en-us/articles/360028720352-Desktop-Access-to-Documents-and-Media", false);
-							}
-
-							String webDavURLInputId = liferayPortletResponse.getNamespace() + "webDavURLInput";
-							%>
-
-							<div class="form-group">
-								<label for="<%= webDavURLInputId %>">
-									<liferay-ui:message key='<%= TextFormatter.format("webDavURL", TextFormatter.K) %>' />
-
-									<liferay-ui:icon-help message="<%= webDavHelpMessage %>" />
-								</label>
-
-								<div class="input-group input-group-sm">
-									<div class="input-group-item input-group-prepend">
-										<input class="form-control" id="<%= webDavURLInputId %>" value="<%= DLURLHelperUtil.getWebDavURL(themeDisplay, fileEntry.getFolder(), fileEntry) %>" />
-									</div>
-
-									<span class="input-group-append input-group-item input-group-item-shrink">
-										<clay:button
-											cssClass="dm-infopanel-copy-clipboard lfr-portal-tooltip"
-											data-clipboard-target='<%= "#" + webDavURLInputId %>'
-											displayType="secondary"
-											icon="paste"
-											title="copy-link"
-										/>
-									</span>
-								</div>
-							</div>
-						</c:if>
-					</div>
+					</c:if>
 				</div>
 			</c:if>
 