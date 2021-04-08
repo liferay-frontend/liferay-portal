29ee0efc49c9535ab1ad406a947265c7ec16b6c7
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2021-04-08 14:28:33 +0200

    LPS-130263 Toolbars should also be configured in the Configcontributor

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/editor/BalloonEditor.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/editor/BalloonEditor.js
@@ -30,28 +30,22 @@ const BalloonEditor = ({config = {}, contents, name, ...otherProps}) => {
 			onInstanceReady={(event) => {
 				const editor = event.editor;
 
-				const balloonToolbars = editor.balloonToolbars;
+				const toolbars = config.toolbars;
 
-				balloonToolbars.create({
-					buttons:
-						'Bold,Italic,Underline,RemoveFormat,Link,NumberedList,BulletedList,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Anchor',
-					cssSelector: 'h1,h2,h3,p,span,strong,li'
-				});
+				if (toolbars) {
+					const balloonToolbars = editor.balloonToolbars;
 
-				balloonToolbars.create({
-					buttons: 'Link,Unlink',
-					priority:
-						window.CKEDITOR.plugins.balloontoolbar.PRIORITY.HIGH,
-					cssSelector: 'a'
-				});
-
-				balloonToolbars.create({
-					buttons:
-						'JustifyLeft,JustifyCenter,JustifyRight,Link,Unlink',
-					priority:
-						window.CKEDITOR.plugins.balloontoolbar.PRIORITY.HIGH,
-					widgets: 'image,image2',
-				});
+					toolbars.forEach((toolbar) => {
+						if (toolbar.priority) {
+							toolbar = {
+								...toolbar, 
+								priority:
+									window.CKEDITOR.plugins.balloontoolbar.PRIORITY.HIGH
+							}
+						}
+						balloonToolbars.create(toolbar);
+					});
+				}
 
 				if (contents) {
 					editor.setData(contents);
