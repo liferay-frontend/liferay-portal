fef7e2203de301c9faed5c7bac174708a2b73d53
garofalog <giulio.garofalo@liferay.com>
2021-06-30 19:55:23 +0200

    COMMERCE-5797 fining addPin on dblclick

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/Diagram.js
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/Diagram.js
@@ -37,6 +37,7 @@ const Diagram = ({
 		handler: false,
 		pin: null,
 	});
+	const [diagramSizes, setDiagramSizes] = useState({ k: 1, x: 0, y:0})
 	const [resetZoom, setResetZoom] = useState(false);
 	const [zoomInHandler, setZoomInHandler] = useState(false);
 	const [zoomOutHandler, setZoomOutHandler] = useState(false);
@@ -203,6 +204,8 @@ const Diagram = ({
 					removePinHandler={removePinHandler}
 					resetZoom={resetZoom}
 					scale={scale}
+					diagramSizes={diagramSizes}
+					setDiagramSizes={setDiagramSizes}
 					selectedOption={selectedOption}
 					setAddPinHandler={setAddPinHandler}
 					setChangedScale={setChangedScale}
