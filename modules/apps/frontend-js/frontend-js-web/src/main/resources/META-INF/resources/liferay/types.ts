66b9c8765de0b6aadd7878ea36c9b036a13cb376
Chema Balsas <jose.balsas@liferay.com>
2021-08-19 13:06:28 +0100

    LPS-X Adds an initial global Liferay interface
    This is going to be arguably the hardest thing to get right in our migration
    to TypeScript. The `Liferay` object is global and augmented from way too
    many different sources making it really hard to type.
    
    This commit aims to start simple and get us to a point where we can start
    adding more types as we go. For now, it simply adds the `Liferay` object
    and describes all the properties defined in `portal-web/top_js.jspf`.
    
    It is left for future commits to:
    - Refine existing types to better match the `Liferay` object
    - Add more types as needed
    - Figure out a better way to handle the `Liferay` object

---
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/types.ts
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+// Properties from `portal-web/top_js.jspf`
+
+export interface ILiferay {
+	AUI: {
+		readonly getCombine: () => boolean;
+		readonly getComboPath: () => string;
+		readonly getDateFormat: () => string;
+		readonly getEditorCKEditorPath: () => string;
+		readonly getFilter: () => string;
+		readonly getFilterConfig: () => string;
+		readonly getJavaScriptRootPath: () => string;
+		readonly getPortletRootPath: () => string;
+		readonly getStaticResourceURLParams: () => string;
+	};
+
+	Browser: {
+		readonly acceptsGzip: () => boolean;
+		readonly getMajorVersion: () => number;
+		readonly getRevision: () => string;
+		readonly getVersion: () => string;
+		readonly isAir: () => boolean;
+		readonly isChrome: () => boolean;
+		readonly isEdge: () => boolean;
+		readonly isFirefox: () => boolean;
+		readonly isGecko: () => boolean;
+		readonly isIe: () => boolean;
+		readonly isIphone: () => boolean;
+		readonly isLinux: () => boolean;
+		readonly isMac: () => boolean;
+		readonly isMobile: () => boolean;
+		readonly isMozilla: () => boolean;
+		readonly isOpera: () => boolean;
+		readonly isRtf: () => boolean;
+		readonly isSafari: () => boolean;
+		readonly isSun: () => boolean;
+		readonly isWebKit: () => boolean;
+		readonly isWindows: () => boolean;
+	};
+
+	Data: {
+		readonly ICONS_INLINE_SVG: boolean;
+		readonly NAV_SELECTOR: string;
+		readonly NAV_SELECTOR_MOBILE: string;
+
+		readonly notices: [{title?: string; message: string; type?: string}];
+
+		readonly isCustomizationView: () => boolean;
+	};
+
+	Language: {
+		readonly available: {[languageId: string]: string};
+		readonly direction: {[languageId: string]: 'ltr' | 'rtl' | null};
+		get: (key: string) => string;
+	};
+
+	PortletKeys: {
+		readonly DOCUMENT_LIBRARY: string;
+		readonly DYNAMIC_DATA_MAPPING: string;
+		readonly ITEM_SELECTOR: string;
+		[key: string]: string | null;
+	};
+
+	PropsValues: {
+		readonly JAVASCRIPT_SINGLE_PAGE_APPLICATION_TIMEOUT: number;
+		readonly NTLM_AUTH_ENABLED: boolean;
+		readonly UPLOAD_SERVLET_REQUEST_IMPL_MAX_SIZE: number;
+		[key: string]: any;
+	};
+
+	ThemeDisplay: {
+		readonly getBCP47LanguageId: () => string;
+		readonly getCanonicalURL: () => string;
+		readonly getCDNBaseURL: () => string;
+		readonly getCDNDynamicResourcesHost: () => string;
+		readonly getCDNHost: () => string;
+		readonly getCompanyGroupId: () => string;
+		readonly getCompanyId: () => string;
+		readonly getDefaultLanguageId: () => string;
+		readonly getDoAsUserIdEncoded: () => string;
+		readonly getLanguageId: () => string;
+		readonly getLayoutId?: () => string;
+		readonly getLayoutRelativeControlPanelURL?: () => string;
+		readonly getLayoutRelativeURL?: () => string;
+		readonly getLayoutURL?: () => string;
+		readonly getParentLayoutId?: () => string;
+		readonly isControlPanel?: () => boolean;
+		readonly isPrivateLayout?: () => boolean;
+		readonly isVirtualLayout?: () => boolean;
+		readonly getParentGroupId: () => string;
+		readonly getPathContext: () => string;
+		readonly getPathImage: () => string;
+		readonly getPathJavaScript: () => string;
+		readonly getPathMain: () => string;
+		readonly getPathThemeImages: () => string;
+		readonly getPathThemeRoot: () => string;
+		readonly getPlid: () => string;
+		readonly getPortalURL: () => string;
+		readonly getScopeGroupId: () => string;
+		readonly getScopeGroupIdOrLiveGroupId: () => string;
+		readonly getSessionId: () => string;
+		readonly getSiteAdminURL: () => string;
+		readonly getSiteGroupId: () => string;
+		readonly getURLControlPanel: () => string;
+		readonly getURLHome: () => string;
+		readonly getUserEmailAddress: () => string;
+		readonly getUserId: () => string;
+		readonly getUserName: () => string;
+		readonly isAddSessionIdToURL: () => boolean;
+		readonly isImpersonated: () => boolean;
+		readonly isSignedIn: () => boolean;
+		readonly isStateExclusive: () => boolean;
+		readonly isStateMaximized: () => boolean;
+		readonly isStatePopUp: () => boolean;
+	};
+
+	readonly authToken: string;
+	readonly currentURL: string;
+	readonly currentURLEncoded: string;
+}
+
+declare global {
+	var Liferay: ILiferay;
+
+	interface Window {
+		Liferay: ILiferay;
+	}
+}
