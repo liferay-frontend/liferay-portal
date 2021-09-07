b23fa19a34a48e12b2539fb197d9fa3a7592f2c9
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-09-07 07:56:55 +0200

    LPS-138464 Use ManagementBarOrderByHandlerBuilder to build ManagementBarOrderByHandler.

--- modules/apps/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserDisplayContext.java
+++ modules/apps/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserDisplayContext.java
@@ -82,8 +82,14 @@ public class AssetBrowserDisplayContext {
 		_renderRequest = renderRequest;
 		_renderResponse = renderResponse;
 
-		_managementBarOrderByHandler = new ManagementBarOrderByHandler(
-			httpServletRequest, AssetBrowserPortletKeys.ASSET_BROWSER);
+		_managementBarOrderByHandler =
+			new ManagementBarOrderByHandler.ManagementBarOrderByHandlerBuilder(
+				httpServletRequest, AssetBrowserPortletKeys.ASSET_BROWSER
+			).defaultOrderByCol(
+				"modified-date"
+			).defaultOrderByType(
+				"asc"
+			).build();
 	}
 
 	public AssetBrowserSearch getAssetBrowserSearch()
@@ -453,11 +459,11 @@ public class AssetBrowserDisplayContext {
 	}
 
 	protected String getOrderByCol() {
-		return _managementBarOrderByHandler.getOrderByCol("modified-date");
+		return _managementBarOrderByHandler.getOrderByCol();
 	}
 
 	protected String getOrderByType() {
-		return _managementBarOrderByHandler.getOrderByType("asc");
+		return _managementBarOrderByHandler.getOrderByType();
 	}
 
 	private long[] _getClassNameIds() {
