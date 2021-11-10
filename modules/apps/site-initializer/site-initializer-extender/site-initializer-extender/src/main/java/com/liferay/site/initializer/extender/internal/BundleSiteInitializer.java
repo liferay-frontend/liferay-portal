de80c080a5602eb80eede801f7e7175739aab62d
Javier de Arcos <javier.dearcos@liferay.com>
2021-11-10 16:04:28 +0100

    LPS-141280 Fix RemoteAppLocalService calls

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1526,11 +1526,13 @@ public class BundleSiteInitializer implements SiteInitializer {
 								jsonObject.getJSONArray("elementURLs")),
 							StringPool.NEW_LINE),
 						"[$", "$]", documentsStringUtilReplaceValues),
-					StringPool.BLANK, jsonObject.getBoolean("instanceable"),
+					Collections.emptyMap(), StringPool.BLANK,
+					jsonObject.getBoolean("instanceable"),
 					_toMap(
 						group.getName(LocaleUtil.getSiteDefault()) + ": ",
 						jsonObject.getString("name_i18n")),
-					jsonObject.getString("portletCategoryName"), sb.toString());
+					jsonObject.getString("portletCategoryName"), sb.toString(),
+					StringPool.BLANK);
 
 			remoteAppEntryIdsStringUtilReplaceValues.put(
 				"REMOTE_APP_ENTRY_ID:" +
