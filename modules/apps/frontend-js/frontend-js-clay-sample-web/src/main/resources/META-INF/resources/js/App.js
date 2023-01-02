f535136f9cfb141fabbeff401a52c02f061cb935
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2023-01-02 14:35:11 +0400

    LPS-168440 - Remove moment.js from clay-sample-web

--- modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
+++ modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
@@ -13,11 +13,17 @@
  */
 
 import ClayAlert from '@clayui/alert';
+import ClayDatePicker from '@clayui/date-picker';
+import {format, getDefaultOptions} from 'date-fns';
 import React from 'react';
 
 import '../css/main.scss';
 
 export default function App() {
+	const [value, setValue] = React.useState('');
+
+	console.log(getDefaultOptions());
+
 	return (
 		<div>
 			<ClayAlert title="Info">
@@ -25,7 +31,26 @@ export default function App() {
 				whatever JS you want to App.js and redeploy.
 			</ClayAlert>
 
-			<div className="clay-test-class">This is where your code goes.</div>
+			<div className="clay-test-class">{format(new Date(), 'PPP')}</div>
+
+			<div className="clay-test-class">{format(new Date(), 'PP')}</div>
+
+			<div className="clay-test-class">{format(new Date(), 'P')}</div>
+
+			<ClayDatePicker
+				ariaLabels={{
+					buttonChooseDate: `Choose Date, selected date is ${
+						value.toLocaleString() ?? value
+					}`,
+					buttonDot: 'Go to today',
+					buttonNextMonth: 'Next month',
+					buttonPreviousMonth: 'Previous month',
+					dialog: 'Choose Date',
+					input: value.toLocaleString(),
+				}}
+				onChange={setValue}
+				value={value}
+			/>
 		</div>
 	);
 }
