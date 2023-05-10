113259365b8973eedafad8ad51ad9d29614994b1
Kevin Tan <kevin.tan@liferay.com>
2023-05-09 17:34:14 -0700

    LPS-176188 LPS-183987 Close dropdown after deleting an item
    Uses `id` if one exists so the row is re-rendered after an item is removed.

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/components/OrderableTable.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/components/OrderableTable.tsx
@@ -320,7 +320,7 @@ const OrderableTable = ({
 										fields={fields}
 										index={index}
 										item={item}
-										key={index}
+										key={item.id || index}
 										onOrderChange={handleOnOrderChange}
 										query={query}
 									/>
