191fbf8735e42cca0b7f3c9b5c5f3899ef0f2bd8
Marko Čikoš <marko.cikos@liferay.com>
2023-05-17 17:51:19 +0200

    LPS-185911 Implement a user-friendly FDS View selection

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/java/com/liferay/frontend/data/set/views/web/internal/fragment/renderer/FDSViewFragmentRenderer.java
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/java/com/liferay/frontend/data/set/views/web/internal/fragment/renderer/FDSViewFragmentRenderer.java
@@ -84,6 +84,8 @@ public class FDSViewFragmentRenderer implements FragmentRenderer {
 							"name", "itemSelector"
 						).put(
 							"type", "itemSelector"
+						).put(
+							"typeOptions", JSONUtil.put("itemType", "FDSView")
 						))))
 		).toString();
 	}
