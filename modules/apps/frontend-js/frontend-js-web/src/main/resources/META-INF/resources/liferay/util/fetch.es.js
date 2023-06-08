05c16f12d4a17984ec0a6287325aa82882bc549b
tototrinh <to.trinh@codeenginestudio.com>
2023-06-08 15:49:10 +0700

    LPS-187019 Make it work if resource is not iterable

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/fetch.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/fetch.es.js
@@ -38,7 +38,7 @@ export default function defaultFetch(resource, init = {}) {
 				resource = resourceLocation;
 			}
 			else {
-				resource = {...resource, url: resourceLocation};
+				Object.assign(resource, {url: resourceLocation});
 			}
 		}
 
@@ -64,7 +64,7 @@ export default function defaultFetch(resource, init = {}) {
 				resource = resourceLocation;
 			}
 			else {
-				resource = {...resource, url: resourceLocation};
+				Object.assign(resource, {url: resourceLocation});
 			}
 		}
 	}
