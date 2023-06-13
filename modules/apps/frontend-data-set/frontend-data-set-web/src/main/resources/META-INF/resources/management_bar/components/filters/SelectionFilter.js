b4375375326e7e47472e9c03d0aa9f6685ddd264
Juanjo Fernández <juanjo.fernandez@liferay.com>
2023-06-13 09:43:56 +0200

    LPS-168812 Inline response mapping
    Map response data to match component API and format code

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/management_bar/components/filters/SelectionFilter.js
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/management_bar/components/filters/SelectionFilter.js
@@ -45,21 +45,6 @@ function fetchData(apiURL, searchParam, currentPage = 1) {
 	}).then((response) => response.json());
 }
 
-const mapResponseData = (data, itemLabel, itemKey) => {
-	return {
-		...data,
-		items: data.items.map((item) => {
-			const option = {label: undefined, value: undefined};
-			option.label = itemLabel
-				? getValueFromItem(item, itemLabel)
-				: item.label;
-			option.value = itemKey ? item[itemKey] : item.value;
-
-			return option;
-		}),
-	};
-};
-
 const getSelectedItemsLabel = ({selectedData}) => {
 	const {exclude, selectedItems} = selectedData;
 
@@ -144,12 +129,14 @@ function SelectionFilter({
 
 			fetchData(apiURL, search, currentPage)
 				.then((response) => {
-					const data = mapResponseData(
-						response,
-						itemLabel,
-						itemKey
-					);
-
+					const selectionItems = response.items.map((item) => {
+						return {
+							label: itemLabel
+								? getValueFromItem(item, itemLabel)
+								: item.label,
+							value: itemKey ? item[itemKey] : item.value,
+						};
+					});
 					if (!isMounted()) {
 						return;
 					}
@@ -157,21 +144,21 @@ function SelectionFilter({
 					setLoading(false);
 
 					if (currentPage === 1) {
-						setItems(data.items);
+						setItems(selectionItems);
 					}
 					else {
-						setItems((items) => [...items, ...data.items]);
+						setItems((items) => [...items, ...selectionItems]);
 					}
 
 					if (
 						firstRequest &&
-						data.totalCount <= DEFAULT_PAGE_SIZE &&
+						response.totalCount <= DEFAULT_PAGE_SIZE &&
 						autocompleteEnabled
 					) {
-						setLocalItems(data.items);
+						setLocalItems(selectionItems);
 					}
 
-					setTotal(data.totalCount);
+					setTotal(response.totalCount);
 					setFirstRequest(false);
 				})
 				.catch(() => {
@@ -182,12 +169,14 @@ function SelectionFilter({
 		}
 		else if (localItems?.length && autocompleteEnabled) {
 			setItems(
-				search ? localItems?.filter(({label}) =>
-					label.toLowerCase().match(search.toLowerCase())
-				) : localItems
+				search
+					? localItems?.filter(({label}) =>
+							label.toLowerCase().match(search.toLowerCase())
+					  )
+					: localItems
 			);
 		}
-	}
+	};
 
 	const debouncedQuery = debounce((value) => {
 		setCurrentPage(1);
