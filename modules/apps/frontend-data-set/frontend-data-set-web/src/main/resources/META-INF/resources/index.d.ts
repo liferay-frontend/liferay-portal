b5632acb487d5b838a1f172a3c2a712badfd6803
Kevin Tan <kevin.tan@liferay.com>
2023-06-15 18:49:27 -0700

    LPS-192593 LPS-192917 frontend-data-set-web: Fix missing prop types

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/index.d.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/index.d.ts
@@ -89,7 +89,14 @@ export interface IItemsActions {
 	label?: string;
 	onClick?: Function;
 	separator?: boolean;
-	target?: 'async' | 'headless' | 'link' | 'modal' | 'sidePanel' | 'event';
+	target?:
+		| 'async'
+		| 'headless'
+		| 'link'
+		| 'modal'
+		| 'modal-permissions'
+		| 'sidePanel'
+		| 'event';
 	type?: string;
 }
 
