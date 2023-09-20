16486aa0e98b24f03f251ca2771c9a9839d7ba00
Krešimir Čoko <kresimir.coko@liferay.com>
2023-09-20 11:42:17 +0200

    LPS-196146 Hide the Creation Actions tab behind the new FF

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/fds_view/Actions.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/fds_view/Actions.tsx
@@ -207,9 +207,11 @@ const Actions = ({fdsView, namespace, spritemap}: IFDSViewSectionInterface) => {
 								{Liferay.Language.get('item-actions')}
 							</ClayTabs.Item>
 
-							<ClayTabs.Item>
-								{Liferay.Language.get('creation-actions')}
-							</ClayTabs.Item>
+							{Liferay.FeatureFlags['LPS-194395'] && (
+								<ClayTabs.Item>
+									{Liferay.Language.get('creation-actions')}
+								</ClayTabs.Item>
+							)}
 						</ClayTabs>
 
 						<ClayTabs.Content active={activeTab} fade>
