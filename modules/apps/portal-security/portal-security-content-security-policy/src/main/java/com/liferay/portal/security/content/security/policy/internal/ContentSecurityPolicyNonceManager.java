a64e177e3d650359061c41a39a32996ab46e0645
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-09-21 16:13:25 +0200

    LPS-192879 Change the way we handle the nonce
    1. We always store a valid nonce in the session
    2. We only set the TLS nonce when CSP is active
    3. When getting the nonce without request we return the one in the TLS
    4. When requesting the nonce with a request we return the one from session or ""
       if CSP is disabled.

--- modules/apps/portal-security/portal-security-content-security-policy/src/main/java/com/liferay/portal/security/content/security/policy/internal/ContentSecurityPolicyNonceManager.java
+++ modules/apps/portal-security/portal-security-content-security-policy/src/main/java/com/liferay/portal/security/content/security/policy/internal/ContentSecurityPolicyNonceManager.java
@@ -5,10 +5,15 @@
 
 package com.liferay.portal.security.content.security.policy.internal;
 
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.SecureRandom;
 import com.liferay.portal.kernel.util.Base64;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.security.content.security.policy.internal.configuration.ContentSecurityPolicyConfiguration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -48,25 +53,23 @@ public class ContentSecurityPolicyNonceManager {
 	}
 
 	public String getNonce(HttpServletRequest httpServletRequest) {
+		if (httpServletRequest == null) {
+			return GetterUtil.getString(_threadLocal.get());
+		}
+
+		ContentSecurityPolicyConfiguration contentSecurityPolicyConfiguration =
+			_getContentSecurityPolicyConfiguration(httpServletRequest);
+
+		if (!contentSecurityPolicyConfiguration.enabled()) {
+			return StringPool.BLANK;
+		}
+
 		httpServletRequest = _portal.getOriginalServletRequest(
 			httpServletRequest);
 
-		String nonce;
+		HttpSession httpSession = httpServletRequest.getSession();
 
-		if (httpServletRequest == null) {
-			nonce = _threadLocal.get();
-		}
-		else {
-			HttpSession httpSession = httpServletRequest.getSession();
-
-			nonce = (String)httpSession.getAttribute(_NONCE);
-		}
-
-		if (nonce == null) {
-			nonce = StringPool.BLANK;
-		}
-
-		return nonce;
+		return GetterUtil.getString(httpSession.getAttribute(_NONCE));
 	}
 
 	public void removeTLSNonce() {
@@ -87,8 +90,32 @@ public class ContentSecurityPolicyNonceManager {
 		return Base64.encode(bytes);
 	}
 
+	private ContentSecurityPolicyConfiguration
+		_getContentSecurityPolicyConfiguration(
+			HttpServletRequest httpServletRequest) {
+
+		try {
+			long groupId = _portal.getScopeGroupId(httpServletRequest);
+
+			if (groupId > 0) {
+				return _configurationProvider.getGroupConfiguration(
+					ContentSecurityPolicyConfiguration.class, groupId);
+			}
+
+			return _configurationProvider.getCompanyConfiguration(
+				ContentSecurityPolicyConfiguration.class,
+				_portal.getCompanyId(httpServletRequest));
+		}
+		catch (PortalException portalException) {
+			return ReflectionUtil.throwException(portalException);
+		}
+	}
+
 	private static final String _NONCE =
 		ContentSecurityPolicyNonceManager.class.getName() + "#NONCE";
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
 
 	@Reference
 	private Portal _portal;
