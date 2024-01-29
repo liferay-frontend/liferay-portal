81a3cf127603a1bcba2e75d67e0c7e4c81f65f0d
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-01-29 13:53:44 +0400

    LPD-15209 - Update portal-company-log-web to ESM

--- modules/apps/portal/portal-company-log-web/npmscripts.config.js
+++ modules/apps/portal/portal-company-log-web/npmscripts.config.js
@@ -1,14 +1,10 @@
-<%--
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
---%>
 
-<%@ include file="/init.jsp" %>
-
-<div class="portal-company-log-web">
-	<react:component
-		module="{App} from portal-company-log-web"
-	/>
-</div>
+module.exports = {
+	build: {
+		main: 'src/main/resources/META-INF/resources/js/index.js',
+	},
+};
