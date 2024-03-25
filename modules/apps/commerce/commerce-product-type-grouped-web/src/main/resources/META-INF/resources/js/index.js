af414aa0d370fb793118f56939b3d7febf4528ce
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-03-25 20:17:07 +0400

    LPD-7837 - Update commerce-product-type-grouped-web to ESM

--- modules/apps/commerce/commerce-product-type-grouped-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/commerce/commerce-product-type-grouped-web/src/main/resources/META-INF/resources/js/index.js
@@ -5,7 +5,10 @@
 
 import {openConfirmModal, openSelectionModal} from 'frontend-js-web';
 
-export default function propsTransformer({portletNamespace, ...props}) {
+export function CPDefinitionGroupedManagementToolbarPropsTransformer({
+	portletNamespace,
+	...props
+}) {
 	return {
 		...props,
 		onActionButtonClick(event, {item}) {
@@ -48,9 +51,10 @@ export default function propsTransformer({portletNamespace, ...props}) {
 							);
 						}
 
-						const addCPDefinitionGroupedEntryFm = document.getElementById(
-							`${portletNamespace}addCPDefinitionGroupedEntryFm`
-						);
+						const addCPDefinitionGroupedEntryFm =
+							document.getElementById(
+								`${portletNamespace}addCPDefinitionGroupedEntryFm`
+							);
 
 						if (addCPDefinitionGroupedEntryFm) {
 							submitForm(addCPDefinitionGroupedEntryFm);
