c9a48801a929af8a1b807b860abdaf73d25c48d2
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-03-25 16:03:35 +0400

    LPD-7837 - Update commerce-product-definitions-web to ESM

---
+++ modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/addCpDefinition.js
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {
+	Autocomplete,
+	CommerceServiceProvider,
+	ModalUtils,
+} from 'commerce-frontend-js';
+import {createPortletURL} from 'frontend-js-web';
+
+export default function main({
+	defaultSku,
+	draft,
+	editProductDefinitionURL,
+	namespace,
+	ppState,
+	productTypeName,
+}) {
+	let defaultLanguageId = null;
+	const productData = {
+		active: true,
+		productStatus: draft,
+		productType: productTypeName,
+	};
+
+	const AdminCatalogResource = CommerceServiceProvider.default.AdminCatalogAPI(
+		'v1'
+	);
+
+	Liferay.provide(window, `${namespace}apiSubmit`, () => {
+		ModalUtils.isSubmitting();
+
+		const formattedData = {...productData, defaultSku, name: {}};
+
+		formattedData.name[defaultLanguageId] = document.getElementById(
+			`${namespace}name`
+		).value;
+
+		AdminCatalogResource.createProduct(formattedData)
+			.then((cpDefinition) => {
+				const redirectURL = createPortletURL(editProductDefinitionURL, {
+					cpDefinitionId: cpDefinition.id,
+					p_p_state: ppState,
+				});
+
+				ModalUtils.closeAndRedirect(redirectURL);
+			})
+			.catch(ModalUtils.onSubmitFail);
+	});
+
+	Autocomplete('autocomplete', 'autocomplete-root', {
+		apiUrl: '/o/headless-commerce-admin-catalog/v1.0/catalogs',
+		inputId: `${namespace}catalogId`,
+		inputName: `${namespace}catalogId`,
+		itemsKey: 'id',
+		itemsLabel: 'name',
+		onValueUpdated(value, catalogData) {
+			if (value) {
+				productData.catalogId = catalogData.id;
+				defaultLanguageId = catalogData.defaultLanguageId;
+			}
+		},
+		required: true,
+	});
+}
