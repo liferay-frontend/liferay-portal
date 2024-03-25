b976b6522b8b7640ce6083fd16d19c2e774191f7
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-03-25 16:02:58 +0400

    LPD-7837 - Update commerce-pricing-web to ESM

---
+++ modules/apps/commerce/commerce-pricing-web/src/main/resources/META-INF/resources/js/addCommerceDiscount.js
@@ -0,0 +1,52 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {ServiceProvider, commerceEvents} from 'commerce-frontend-js';
+import {createPortletURL} from 'frontend-js-web';
+
+export default function ({
+	editCommerceDiscountRenderURL,
+	level,
+	limitationType,
+	namespace,
+}) {
+	const CommerceDiscountResource = ServiceProvider.AdminPricingAPI('v2');
+
+	Liferay.provide(window, `${namespace}apiSubmit`, () => {
+		const discountData = {
+			level,
+			limitationType,
+			target: document.getElementById('commerceDiscountTarget').value,
+			title: document.getElementById('title').value,
+			usePercentage: document.getElementById('commerceDiscountType')
+				.value,
+		};
+
+		return CommerceDiscountResource.addDiscount(discountData)
+			.then((payload) => {
+				const redirectURL = createPortletURL(
+					editCommerceDiscountRenderURL,
+					{
+						commerceDiscountId: payload.id,
+						p_auth: Liferay.authToken,
+						usePercentage: payload.usePercentage,
+					}
+				);
+
+				window.parent.Liferay.fire(commerceEvents.CLOSE_MODAL, {
+					redirectURL,
+					successNotification: {
+						message: Liferay.Language.get(
+							'your-request-completed-successfully'
+						),
+						showSuccessNotification: true,
+					},
+				});
+			})
+			.catch((error) => {
+				return Promise.reject(error);
+			});
+	});
+}
