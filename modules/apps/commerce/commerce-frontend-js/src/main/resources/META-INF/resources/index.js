6c956930e9020c274a45b67c72d50be6d581149c
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-03-25 16:00:28 +0400

    LPD-7837 - Update commerce-frontend-js to ESM

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/index.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/index.js
@@ -3,7 +3,23 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export * as CommerceComponents from './components/index';
+export {
+	AddToCart,
+	AddToWishList,
+	Autocomplete,
+	AutocompletePureComponent,
+	DropdownMenu,
+	Gallery,
+	ItemFinder,
+	MiniCart,
+	Price,
+	QuantitySelector,
+	StepTracker,
+	Summary,
+	TierPrice,
+	UnitOfMeasureSelector,
+	MiniCartContext,
+} from './components/index';
 export {default as CommerceServiceProvider} from './ServiceProvider/index';
 export {default as CommerceFrontendUtils} from './utilities/interface/index';
 
@@ -12,3 +28,23 @@ export {default as CommerceFrontendUtils} from './utilities/interface/index';
 // instead.
 
 export const CommerceContext = Liferay.CommerceContext;
+
+export {default as commerceEvents} from './utilities/eventsDefinitions';
+export {default as FormUtils} from './utilities/forms/index';
+export {default as frontendJsWeb} from 'frontend-js-web';
+export {default as MiniCompare} from './components/mini_compare/entry';
+export {default as slugify} from './utilities/slugify';
+export {
+	fetchHeaders,
+	getData,
+	liferayNavigate,
+	getObjectFromPath,
+	formatAutocompleteItem,
+	getValueFromItem,
+	getLabelFromItem,
+	formatActionUrl,
+	getRandomId,
+	sortByKey,
+	isProductPurchasable,
+} from './utilities/index';
+export {default as ModalUtils} from './utilities/modals/index';
