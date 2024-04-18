0fb368fd6b9ac000762414903b20480816683d7e
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-04-18 13:58:10 +0400

    LPD-23370 - AUI files are no longer processed through babel

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/modules.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/modules.js
@@ -23,7 +23,7 @@
 			'/aui/',
 		combine: COMBINE,
 		comboBase: LiferayAUI.getComboPath(),
-		filter: process.env.NODE_ENV === 'development' ? 'raw' : 'min',
+		filter: 'min', // If you need to debug this file, replace this with 'raw'
 		groups: {
 			editor: {
 				base: PATH_EDITOR_CKEDITOR,
@@ -165,8 +165,7 @@
 						plugins: {
 							'liferay-navigation-interaction-touch': {
 								condition: {
-									name:
-										'liferay-navigation-interaction-touch',
+									name: 'liferay-navigation-interaction-touch',
 									test(A) {
 										return A.UA.touchEnabled;
 									},
