ab3a3593359eccdc82f6ef3527044f336e78a0a6
Tina Tian <tina.tian@liferay.com>
2024-05-22 13:36:37 -0700

    Revert "LPD-20946 Pass id when explicitly calling deleteObjectDefinition() in integration tests, to avoid the cases where an object definition is created, assigned to a variable, and then published but not re-assigned to that variable, causing a incomplete deletion using an out-dated instance."
    This reverts commit 6a13c28c08b5be642cac49eedfc16a7328097fba.

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -439,8 +439,7 @@ public class ObjectEntryLocalServiceTest {
 		// reference a required list type entry cannot be deleted before it is
 		// unreferenced
 
-		_objectDefinitionLocalService.deleteObjectDefinition(
-			_objectDefinition.getObjectDefinitionId());
+		_objectDefinitionLocalService.deleteObjectDefinition(_objectDefinition);
 	}
 
 	@Test
@@ -873,8 +872,7 @@ public class ObjectEntryLocalServiceTest {
 					).build()
 				).build()));
 
-		_objectDefinitionLocalService.deleteObjectDefinition(
-			objectDefinition.getObjectDefinitionId());
+		_objectDefinitionLocalService.deleteObjectDefinition(objectDefinition);
 	}
 
 	@Test
@@ -2365,9 +2363,9 @@ public class ObjectEntryLocalServiceTest {
 		_assertCount(0);
 
 		_objectDefinitionLocalService.deleteObjectDefinition(
-			draftObjectDefinition.getObjectDefinitionId());
+			draftObjectDefinition);
 		_objectDefinitionLocalService.deleteObjectDefinition(
-			publishedObjectDefinition.getObjectDefinitionId());
+			publishedObjectDefinition);
 	}
 
 	@Test
@@ -3886,8 +3884,7 @@ public class ObjectEntryLocalServiceTest {
 			Assert.assertEquals(1, baseModelSearchResult.getLength());
 		}
 
-		_objectDefinitionLocalService.deleteObjectDefinition(
-			objectDefinition.getObjectDefinitionId());
+		_objectDefinitionLocalService.deleteObjectDefinition(objectDefinition);
 	}
 
 	private void _testUpdateObjectEntryExternalReferenceCode()
