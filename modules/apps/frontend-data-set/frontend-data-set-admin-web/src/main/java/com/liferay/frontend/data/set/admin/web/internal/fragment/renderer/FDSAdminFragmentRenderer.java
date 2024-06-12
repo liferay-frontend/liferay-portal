8799a1ccdab1fc7300fa17b3e9a2fcad4210519d
Kevin Tan <kevin.tan@liferay.com>
2024-06-11 19:03:18 -0700

    LPD-25463 frontend-data-set-views-web: Fallback to site default translation

--- modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/java/com/liferay/frontend/data/set/admin/web/internal/fragment/renderer/FDSAdminFragmentRenderer.java
+++ modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/java/com/liferay/frontend/data/set/admin/web/internal/fragment/renderer/FDSAdminFragmentRenderer.java
@@ -1008,6 +1008,16 @@ public class FDSAdminFragmentRenderer implements FragmentRenderer {
 			(ObjectEntry objectEntry) -> {
 				Map<String, Object> properties = objectEntry.getProperties();
 
+				String label = (String)properties.get("label");
+
+				if (label.equals(StringPool.BLANK)) {
+					Map<String, String> labelI18n =
+						(Map<String, String>)properties.get("label_i18n");
+
+					label = labelI18n.get(
+						LocaleUtil.toLanguageId(LocaleUtil.getSiteDefault()));
+				}
+
 				if (FeatureFlagManagerUtil.isEnabled("LPD-19465")) {
 					return JSONUtil.put(
 						"active", properties.get("default")
@@ -1018,7 +1028,7 @@ public class FDSAdminFragmentRenderer implements FragmentRenderer {
 					).put(
 						"key", properties.get("fieldName")
 					).put(
-						"label", properties.get("label")
+						"label", label
 					);
 				}
 
