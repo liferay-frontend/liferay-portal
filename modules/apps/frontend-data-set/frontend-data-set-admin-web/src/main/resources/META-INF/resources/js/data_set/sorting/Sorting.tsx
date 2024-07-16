8b9d73bb7db407083ee41d554f40650b707bb5fd
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2024-07-16 15:04:14 +0200

    LPD-25463 frontend-data-set-views-web: DSM Sorting tab labels fallback to default language

--- modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/data_set/sorting/Sorting.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/data_set/sorting/Sorting.tsx
@@ -12,6 +12,7 @@ import ClayLoadingIndicator from '@clayui/loading-indicator';
 import ClayModal from '@clayui/modal';
 import {InputLocalized} from 'frontend-js-components-web';
 import {fetch, openModal} from 'frontend-js-web';
+import fuzzy from 'fuzzy';
 import React, {useEffect, useState} from 'react';
 
 import {IDataSet} from '../../DataSets';
@@ -20,6 +21,7 @@ import OrderableTable from '../../components/OrderableTable';
 import RequiredMark from '../../components/RequiredMark';
 import {
 	API_URL,
+	FUZZY_OPTIONS,
 	OBJECT_RELATIONSHIP,
 	OBJECT_RELATIONSHIP_LEGACY,
 	OBJECT_RELATIONSHIP_NEW,
@@ -70,6 +72,37 @@ const DefaultComponent = ({item}: IContentRendererProps) => {
 				? Liferay.Language.get('yes')
 				: Liferay.Language.get('no')}
 		</ClayLabel>
+	);
+};
+
+const LabelComponent = ({item, query}: IContentRendererProps) => {
+	const label =
+		item.label ||
+		item.label_i18n[Liferay.ThemeDisplay.getDefaultLanguageId()] ||
+		'';
+
+	const fuzzyMatch = fuzzy.match(query, label, FUZZY_OPTIONS);
+
+	return (
+		<span className="table-list-title">
+			{fuzzyMatch ? (
+				<span
+					dangerouslySetInnerHTML={{
+						__html: fuzzyMatch.rendered,
+					}}
+				/>
+			) : (
+				<span>{label}</span>
+			)}
+		</span>
+	);
+};
+
+const labelTextMatch = (item: ISort) => {
+	return (
+		item.label ||
+		item.label_i18n[Liferay.ThemeDisplay.getDefaultLanguageId()] ||
+		''
 	);
 };
 
@@ -649,7 +682,10 @@ const Sorting = ({
 						]}
 						fields={[
 							{
-								headingTitle: true,
+								contentRenderer: {
+									component: LabelComponent,
+									textMatch: labelTextMatch,
+								},
 								label: Liferay.Language.get('label'),
 								name: 'label',
 							},
