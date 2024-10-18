713e446dfbce7d8352d08b5a79842683732d15b6
Kevin Tan <kevin.tan@liferay.com>
2024-10-17 22:10:32 -0700

    LPD-37313 Use ClaySamplePage on panelGroupTag and verticalNavTag tests

--- modules/test/playwright/tests/frontend-taglib-clay/panelGroupTag.spec.ts
+++ modules/test/playwright/tests/frontend-taglib-clay/panelGroupTag.spec.ts
@@ -9,13 +9,11 @@ import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../fixtures/loginTest';
-import {liferayConfig} from '../../liferay.config';
-import getRandomString from '../../utils/getRandomString';
-import getPageDefinition from '../layout-content-page-editor-web/utils/getPageDefinition';
-import getWidgetDefinition from '../layout-content-page-editor-web/utils/getWidgetDefinition';
+import {claySamplePageTest} from './fixtures/claySamplePageTest';
 
 export const test = mergeTests(
 	apiHelpersTest,
+	claySamplePageTest,
 	featureFlagsTest({
 		'LPS-178052': true,
 	}),
@@ -26,34 +24,16 @@ export const test = mergeTests(
 test(
 	'Check role and aria-orientation is removed',
 	{tag: '@LPD-30368'},
-	async ({apiHelpers, page, site}) => {
-		let layout: Layout;
+	async ({apiHelpers, claySamplePage, page, site}) => {
+		await test.step('Create a content site and the clay sample widget', async () => {
+			await claySamplePage.setupClaySampleWidget({apiHelpers, site});
+		});
 
-		await test.step('Create a content site and the frontend taglib clay widget', async () => {
-			const widgetDefinition = getWidgetDefinition({
-				id: getRandomString(),
-				widgetName:
-					'com_liferay_clay_sample_web_portlet_ClaySamplePortlet',
-			});
-
-			layout = await apiHelpers.headlessDelivery.createSitePage({
-				pageDefinition: getPageDefinition([widgetDefinition]),
-				siteId: site.id,
-				title: getRandomString(),
-			});
+		await test.step('Select Panel tab', async () => {
+			await claySamplePage.selectTab('Panel');
 		});
 
 		await test.step('Check that role and aria-orientation is not present', async () => {
-			await page.goto(
-				`${liferayConfig.environment.baseUrl}/web${site.friendlyUrlPath}${layout.friendlyUrlPath}`
-			);
-
-			const panelTab = page.getByRole('tablist').getByText('Panel');
-
-			await panelTab.waitFor({state: 'visible'});
-
-			await panelTab.click();
-
 			const panelGroup = page.getByRole('heading', {name: 'PANEL GROUP'});
 
 			await panelGroup.waitFor({state: 'visible'});
