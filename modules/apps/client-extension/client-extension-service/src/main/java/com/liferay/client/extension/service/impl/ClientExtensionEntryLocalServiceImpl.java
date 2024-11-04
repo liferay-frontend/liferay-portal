ec3bdc77c4e232291974146eb756d10c80d5ffb6
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-11-04 13:22:20 +0100

    LPD-41116 Remove suspicious invocation through local service proxy
    Commit 4c1c8f4e74bd88be6ea0069bf52e785135de48a3 introduced the
    addOrUpdateClientExtensionEntry() method and it already invoked
    updateClientExtensionEntry() via clientExtensionEntryLocalService,
    whereas addClientExtensionEntry() was invoked directly.
    
    I believe this was an error so I'm removing it in case it can create
    future problems because one method is invoked through the proxy while
    the other one is invoked directly in the very same object instance.

--- modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/service/impl/ClientExtensionEntryLocalServiceImpl.java
+++ modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/service/impl/ClientExtensionEntryLocalServiceImpl.java
@@ -125,7 +125,7 @@ public class ClientExtensionEntryLocalServiceImpl
 					externalReferenceCode, user.getCompanyId());
 
 		if (clientExtensionEntry != null) {
-			return clientExtensionEntryLocalService.updateClientExtensionEntry(
+			return updateClientExtensionEntry(
 				userId, clientExtensionEntry.getClientExtensionEntryId(),
 				description, nameMap, properties, sourceCodeURL, typeSettings);
 		}
