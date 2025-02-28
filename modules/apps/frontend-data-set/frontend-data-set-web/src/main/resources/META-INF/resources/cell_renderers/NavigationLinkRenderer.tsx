ae2915684208008e736e9e26423c7296ec1f8d7d
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2025-02-28 09:38:08 +0100

    Add fallback renderer

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/cell_renderers/NavigationLinkRenderer.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/cell_renderers/NavigationLinkRenderer.tsx
@@ -1,46 +1,50 @@
 /**
- * SPDX-FileCopyrightText: (c) 20256 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayIcon from '@clayui/icon';
 import ClayLink from '@clayui/link';
-import React, {useContext} from 'react';
-
-// @ts-ignore
+import React from 'react';
 
 import DefaultContent from './DefaultRenderer';
-import FrontendDataSetContext from '../FrontendDataSetContext';
-import ClayIcon from '@clayui/icon';
 
 const NavigationLinkRenderer = ({
 	itemData,
-	itemId,
-	options,
-	value
+	openFolder,
+	options = {},
+	value,
 }: {
 	itemData: any;
-	itemId: number;
+	openFolder: ({item}: {item: any}) => void;
 	options: any;
 	value: string;
 }) => {
-	const {openFolder} = useContext(FrontendDataSetContext);
 	const isFolder = itemData?.entryClassName.includes('Folder');
+
 	return (
 		<div className="table-list-title">
-			<ClayLink
-				href='#'
-				onClick={(event) =>{
-					event.preventDefault();
+			{isFolder ? (
+				<ClayLink
+					href="#"
+					onClick={(event) => {
+						event.preventDefault();
 
-					openFolder({item: itemData});
-				}}
-				style={{cursor: 'pointer'}}
-			>
-				{isFolder && <span className='c-pr-2'>
-					<ClayIcon symbol='folder' />
-					</span>}
-				{value}
-			</ClayLink>
+						openFolder({item: itemData});
+					}}
+					style={{cursor: 'pointer'}}
+				>
+					{isFolder && (
+						<span className="c-pr-2">
+							<ClayIcon symbol="folder" />
+						</span>
+					)}
+
+					{value}
+				</ClayLink>
+			) : (
+				<DefaultContent options={options} value={value} />
+			)}
 		</div>
 	);
 };
