f83c0b5b6704a6263c1f8f66dd71c1790137d0ac
Marco Leo <marco.leo@liferay.com>
2025-03-10 00:18:32 +0100

    LPD-42278 Filter out actions that doesnt match

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/index.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/index.ts
@@ -120,6 +120,7 @@ export interface IItemActionsData {
 	confirmationMessage?: string;
 	disableHeader?: boolean;
 	errorMessage?: string;
+	filters?: IItemActionsDataFilter[];
 	id?: string | number;
 	method?: 'delete' | 'get' | 'patch' | 'post';
 	permissionKey?: string;
@@ -128,6 +129,11 @@ export interface IItemActionsData {
 	status?: ModalStatus;
 	successMessage?: string;
 	title?: string;
+}
+
+export interface IItemActionsDataFilter {
+	key: string;
+	value: string;
 }
 
 export interface IQuickActions extends IBaseActions {
