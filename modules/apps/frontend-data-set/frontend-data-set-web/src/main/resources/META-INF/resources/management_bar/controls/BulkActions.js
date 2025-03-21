dabb0d2102822d0fb54218a000fb778ae43e65c0
Kevin Tan <kevin.tan@liferay.com>
2025-03-21 16:25:05 -0700

    LPD-44871 Add responsiveness with icon only buttons

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/management_bar/controls/BulkActions.js
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/management_bar/controls/BulkActions.js
@@ -237,11 +237,11 @@ function BulkActions({
 
 										return (
 											<li
-												className="nav-item"
+												className="d-none d-sm-flex nav-item"
 												key={actionDefinition.data.id}
 											>
 												<ClayButton
-													className="nav-link"
+													className="d-lg-inline d-none nav-link"
 													displayType="unstyled"
 													onClick={() =>
 														handleActionClick(
@@ -264,6 +264,30 @@ function BulkActions({
 
 													{actionDefinition.label}
 												</ClayButton>
+
+												<ClayButtonWithIcon
+													aria-label={
+														actionDefinition.label
+													}
+													className="d-lg-none nav-link nav-link-monospaced"
+													displayType="unstyled"
+													onClick={() =>
+														handleActionClick(
+															actionDefinition,
+															formId,
+															formName,
+															loadData,
+															namespace,
+															sidePanelId
+														)
+													}
+													symbol={
+														actionDefinition.icon
+													}
+													title={
+														actionDefinition.label
+													}
+												/>
 											</li>
 										);
 									}
