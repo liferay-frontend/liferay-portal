c1b3bf1fd048f823789522156221c9427b6c5cb3
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2025-06-27 13:01:12 +0200

    LPD-51442 - Fix types

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/types.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/types.ts
@@ -218,7 +218,7 @@ export interface IHeader {
 }
 
 export interface IListTitleRenderer {
-	component: ({itemData}: {itemData: any}) => React.ReactNode;
+	component: ({itemData}: {itemData: any}) => JSX.Element;
 }
 
 export interface IListSchema {
