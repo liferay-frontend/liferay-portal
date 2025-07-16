fd1975ddd98a8797b55daf59bfa457dc9d1de059
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2025-07-16 09:33:48 +0200

    LPD-55419 - Refactor. Replace usages of `item[selectedItemsKey]`

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/views/cards/Cards.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/views/cards/Cards.tsx
@@ -15,6 +15,7 @@ import useFDSDrop from '../../dnd/useFDSDrop';
 import filterItemActions from '../../utils/actionItems/filterItemActions';
 import formatActionURL from '../../utils/actionItems/formatActionURL';
 import handleActionClick from '../../utils/actionItems/handleActionClick';
+import getItemValueFromSelectedItemsKey from '../../utils/getItemValueFromSelectedItemsKey';
 import {getLocalizedValue} from '../../utils/getLocalizedValue';
 import getRandomId from '../../utils/getRandomId';
 import isLink from '../../utils/isLink';
@@ -73,7 +74,9 @@ const Card = forwardRef<HTMLDivElement, any>(
 				selectedItemsValue,
 			}) as any);
 
-		const selectedItemKey = selectedItemsKey && item[selectedItemsKey];
+		const selectedItemKey =
+			selectedItemsKey &&
+			getItemValueFromSelectedItemsKey(item, selectedItemsKey);
 
 		const getLabels = (
 			item: any
@@ -178,7 +181,12 @@ const Card = forwardRef<HTMLDivElement, any>(
 				selectable &&
 				!!selectedItemsValue?.find(
 					(element) =>
-						selectedItemsKey && element === item[selectedItemsKey]
+						selectedItemsKey &&
+						element ===
+							getItemValueFromSelectedItemsKey(
+								item,
+								selectedItemsKey
+							)
 				),
 			stickerProps: (schema.sticker && item[schema.sticker]) || null,
 			symbol: schema.symbol && item[schema.symbol],
@@ -259,7 +267,10 @@ const Cards = ({
 								item={item}
 								key={
 									selectedItemsKey
-										? item[selectedItemsKey]
+										? getItemValueFromSelectedItemsKey(
+												item,
+												selectedItemsKey
+											)
 										: getRandomId()
 								}
 								onItemSelectionChange={onItemSelectionChange}
