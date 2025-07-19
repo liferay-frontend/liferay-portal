7d968f0a254ac5467cb5cb9843e43f21a91bff62
Kevin Tan <kevin.tan@liferay.com>
2025-07-18 18:55:01 -0700

    LPD-60969 Create new portal-impl playwright project

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -97,6 +97,7 @@ import {config as passwordPoliciesAdminWebFirstLoginConfig} from './tests/passwo
 import {config as passwordPoliciesAdminWebConfig} from './tests/password-policies-admin-web/main/config';
 import {config as passwordPoliciesAdminWebSetupAdminConfig} from './tests/password-policies-admin-web/setup-admin/config';
 import {config as portalDefaultPermissionsWebConfig} from './tests/portal-default-permissions-web/main/config';
+import {config as portalImplPortletConfig} from './tests/portal-impl/portlet/config';
 import {config as portalLanguageOverrideWebConfig} from './tests/portal-language-override-web/main/config';
 import {config as portalSearchAdminWebConfig} from './tests/portal-search-admin-web/main/config';
 import {config as portalSearchWebConfig} from './tests/portal-search-web/main/config';
@@ -257,6 +258,7 @@ export default defineConfig({
 		passwordPoliciesAdminWebFirstLoginConfig,
 		passwordPoliciesAdminWebSetupAdminConfig,
 		portalDefaultPermissionsWebConfig,
+		portalImplPortletConfig,
 		portalLanguageOverrideWebConfig,
 		portalSearchAdminWebConfig,
 		portalSearchWebConfig,
