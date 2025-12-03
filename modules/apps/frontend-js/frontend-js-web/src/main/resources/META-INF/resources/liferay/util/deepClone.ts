41f60bc21f8ece1b39de4464e510554f1551ce61
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2025-12-03 15:51:11 +0100

    LPD-9465 - Refactor. Simplify null value handling

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/deepClone.ts
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/deepClone.ts
@@ -10,10 +10,6 @@
  * @returns A deep clone of the object.
  */
 export default function deepClone(object: any) {
-	if (object === null) {
-		return object;
-	}
-
 	if (Array.isArray(object)) {
 		return object.map((item): any => deepClone(item));
 	}
@@ -26,7 +22,7 @@ export default function deepClone(object: any) {
 		return new RegExp(object.source, object.flags);
 	}
 
-	if (typeof object === 'object') {
+	if (object && typeof object === 'object') {
 		const newObject: Record<string, any> = {};
 
 		Object.keys(object).forEach((key) => {
