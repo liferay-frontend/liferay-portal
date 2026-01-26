ab20dfc2c8c4c8cdeb10c377cbe9ab014c0a50c2
antonio-ortega <antonio.ortega@liferay.com>
2026-01-26 13:32:26 +0100

    LPD-73128 Fix condition to avoid adding filter params to URL when reloading the page

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/FrontendDataSet.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/FrontendDataSet.tsx
@@ -782,7 +782,10 @@ const FrontendDataSetContent = ({
 
 				if (
 					preloadedData !== selectedData ||
-					(preloadedData === selectedData && configInURL)
+					(preloadedData === selectedData &&
+						configInURL?.filters?.some(
+							(obj) => obj.id === filter.id
+						))
 				) {
 					updateConfigInURL({
 						[EConfigInURLKeys.ACTIVE_FILTERS]:
