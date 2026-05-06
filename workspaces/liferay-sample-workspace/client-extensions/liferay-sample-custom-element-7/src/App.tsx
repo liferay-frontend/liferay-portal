672f4447ee9441578afe33a324c37ce0a523ea5c
antonio-ortega <antonio.ortega@liferay.com>
2026-05-06 11:05:21 +0200

    LPD-86378 Use js-api instead of global Liferay.State object

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-custom-element-7/src/App.tsx
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-custom-element-7/src/App.tsx
@@ -3,79 +3,60 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {FDSState, getFDSAtom} from '@liferay/js-api/data-set';
+import {Atom, readAtom, subscribeAtom, writeAtom} from '@liferay/js-api/state';
 import React, {useEffect, useState} from 'react';
-
-import {IFDSState, Liferay} from './services/liferay';
 
 interface AppProps {
 	fdsName: string;
 }
 
-const ATOM_POLL_INTERVAL_MS = 100;
-
-const getAtomKey = (fdsName: string) => `${fdsName}_fdsState`;
-
 function App({fdsName}: AppProps) {
-	const atomKey = getAtomKey(fdsName);
-
+	const [atom, setAtom] = useState<Atom<FDSState> | null>(null);
 	const [query, setQuery] = useState('');
-	const [ready, setReady] = useState(
-		() => Liferay.State.__unsafe__.getAtomOrSelectorKey(atomKey) !== null
-	);
 
 	useEffect(() => {
-		if (ready) {
-			return;
-		}
+		let disposed = false;
+		let subscription: {dispose: () => void} | undefined;
 
-		const intervalId = window.setInterval(() => {
-			if (Liferay.State.__unsafe__.getAtomOrSelectorKey(atomKey)) {
-				setReady(true);
-			}
-		}, ATOM_POLL_INTERVAL_MS);
+		getFDSAtom(fdsName)
+			.then((resolvedAtom) => {
+				if (disposed) {
+					return;
+				}
 
-		return () => window.clearInterval(intervalId);
-	}, [atomKey, ready]);
+				setAtom(resolvedAtom);
+				setQuery(readAtom(resolvedAtom).search?.query ?? '');
 
-	useEffect(() => {
-		if (!ready) {
-			return;
-		}
+				subscription = subscribeAtom(resolvedAtom, (next) => {
+					setQuery(next?.search?.query ?? '');
+				});
+			})
+			.catch((error: Error) => {
+				console.warn(
+					`[liferay-sample-custom-element-7] ${error.message}`
+				);
+			});
 
-		const initialState = Liferay.State.__unsafe__.readKey<IFDSState>(
-			atomKey
-		);
-
-		setQuery(initialState?.search?.query ?? '');
-
-		const subscription = Liferay.State.__unsafe__.subscribeKey<IFDSState>(
-			atomKey,
-			(nextState) => setQuery(nextState?.search?.query ?? '')
-		);
-
-		return () => subscription.dispose();
-	}, [atomKey, ready]);
+		return () => {
+			disposed = true;
+			subscription?.dispose();
+		};
+	}, [fdsName]);
 
 	const handleSearch = () => {
-		const currentState = Liferay.State.__unsafe__.readKey<IFDSState>(
-			atomKey
-		);
-
-		if (!currentState) {
+		if (!atom) {
 			return;
 		}
 
-		Liferay.State.__unsafe__.writeKey<IFDSState>(atomKey, {
-			...currentState,
-			search: {query},
-		});
+		writeAtom(atom, {...readAtom(atom), search: {query}});
 	};
 
 	return (
 		<div className="liferay-sample-custom-element-7-search">
 			<input
 				className="form-control"
-				disabled={!ready}
+				disabled={!atom}
 				onChange={(event) => setQuery(event.target.value)}
 				onKeyDown={(event) => {
 					if (event.key === 'Enter') {
@@ -83,7 +64,7 @@ function App({fdsName}: AppProps) {
 					}
 				}}
 				placeholder={
-					ready
+					atom
 						? `Search in ${fdsName}`
 						: `Waiting for FDS "${fdsName}"...`
 				}
@@ -93,7 +74,7 @@ function App({fdsName}: AppProps) {
 
 			<button
 				className="btn btn-primary"
-				disabled={!ready}
+				disabled={!atom}
 				onClick={handleSearch}
 				type="button"
 			>
